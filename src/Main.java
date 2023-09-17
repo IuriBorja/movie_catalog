34587e73964ef417a407efc3bc0f1883ff388de1
Iuri Borja <95659045+IuriBorja@users.noreply.github.com>
2023-09-17 14:38:28 +0100

    att movie class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Movie movie1 = new Movie();
+        movie1.name = "Interestellar";
+        movie1.showDatasheet();
 
     }
 
